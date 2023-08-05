8240aa78248d68ad6305762f4b03902b0f91e77a
yeyderjhjl <jturpoan.@unsa.edu.pe>
2023-08-05 00:09:13 -0500

    impl4mementación modelos

--- tiendaAqp/cafeteria/models.py
+++ tiendaAqp/cafeteria/models.py
@@ -2,3 +2,71 @@ from django.db import models
 
 # Create your models here.
 
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    ESTADOS_PEDIDO = [
+        ('P', 'Pendiente'),
+        ('E', 'En preparación'),
+        ('C', 'Completado'),
+    ]
+    estado = models.CharField(max_length=1, choices=ESTADOS_PEDIDO, default='P')
+
+    def __str__(self):
+        return f"Pedido #{self.id}"
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Pedido {self.producto.nombre} - {self.cantidad} - {self.precio_unitario}"
+
+class Carrito(models.Model):
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto, through='ItemCarrito')
+
+    def __str__(self):
+        return f"Carrito de {self.usuario.username}"
+    
+class ItemCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.cantidad} de {self.producto.nombre} en {self.carrito}"
+    
+class ReservaMesa(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    cantidad_personas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Reserva para {self.cantidad_personas} personas el {self.fecha} a las {self.hora}"
